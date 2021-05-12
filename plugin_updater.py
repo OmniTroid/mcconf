c5cefcb3b93a177913aa0076ecd058563b073712
David Skoland <davidskoland@gmail.com>
2021-05-13 01:31:48 +0200

    add more logic to plugin downloader

--- plugin_updater.py
+++ plugin_updater.py
@@ -1,44 +1,83 @@
 import requests
 import config as c
+import os
 
 api_url = 'https://api.spiget.org/v2/resources/{resource_id}'
 version_url = api_url + '/versions/latest'
-download_url = api_url + '/download'
+download_url = api_url + '/versions/{resource_version_id}/download'
 
-resource_ids = {
-	'EssentialsX': '9089'
+resource_metadata = {
+	'EssentialsX': {'id': '9089', 'filetype': 'zip'}
 }
+
+valid_filetypes = {
+	'zip',
+	'jar'
+}
+
+def make_request(url : str, expected_status_code = 200) -> requests.Response:
+	headers = {'User-Agent': 'mcconf plugin updater'}
+
+	response = requests.get(url, headers = headers)
+
+	if response.status_code != expected_status_code:
+		print('Web request failed: ' + url)
+		print('Expected code: ' + str(expected_status_code))
+		print('Received code: ' + str(response.status_code))
+		return None
+
+	return response
 
 # Downloads specified resource to current directory
 def download_resource(resource_name : str):
 	cwd = os.getcwd()
 	os.chdir(c.plugin_dir)
-	resource_id = resource_ids[resource_name]
+	resource_id = resource_metadata[resource_name]['id']
 
-	headers = {'User-Agent': 'mcconf plugin updater'}
+	# Get data about the latest version
+	version_response = make_request(version_url.format(resource_id = resource_id))
+	if version_response == None:
+		return
 
-	version_response = requests.get(version_url.format(resource_id = resource_id), headers=headers)
+	version_response_json = version_response.json()
+	resource_version = version_response_json['name']
+	resource_version_id = version_response_json['id']
 
-	response = requests.get(api_url.format(resource_id = resource_id), headers=headers)
+	filename = f'{resource_name}-{resource_version}'
 
-	print(response.headers)
+	# Check if we have the exact version already
+	for filetype in valid_filetypes:
+		if os.path.exists(filename + '.' + filetype):
+			print('File already exists: ' + os.getcwd() + filename + '.' + filetype)
+			return
 
-	if response.status_code != 200:
-		print('Failed to download resource ' + resource_name)
+	download_response = make_request(download_url.format(
+		resource_id = resource_id,
+		resource_version_id = resource_version_id))
+
+	if download_response == None:
 		return
 
 	filetype = ''
 
 	if headers['Content-Type'] == 'application/zip':
 		filetype = 'zip'
-	else
+	else:
 		print('Unknown Content-Type: ' + headers['Content-Type'])
 		return
 
-	outfile = open(filename + '.' + filetype, 'wb')
+	if not path.exists(resource_name):
+		os.mkdir(resource_name)
+
+	os.chdir(resource_name)
+
+	filename = f'{filename}.{filetype}'
+
+	outfile = open(filename, 'wb')
 	outfile.write(response.content)
 	outfile.close()
 
+	#if filetype == 'zip'
 
 
 
