c39b2701c9572f762475806661e54155c016aeb6
David Skoland <davidskoland@gmail.com>
2022-04-16 12:19:43 +0200

    Reformat test_trivial.py

--- fs2conf/test_trivial.py
+++ fs2conf/test_trivial.py
@@ -1,23 +1,25 @@
 from pathlib import Path
-import os
+
 import pprint as pp
 
 import fs2conf as f2c
 
+
 def test_trivial():
 
-	root_path = Path('')
+    root_path = Path('')
 
-	result = f2c.fs2conf(dir_=root_path)
+    result = f2c.fs2conf(dir_=root_path)
 
-	pp.pprint(result)
+    pp.pprint(result)
 
-	assert True
+    assert True
+
 
 def test_empty():
-	empty_folder = Path('/tmp/fs2conf_empty')
-	empty_folder.mkdir(exist_ok=True)
+    empty_folder = Path('/tmp/fs2conf_empty')
+    empty_folder.mkdir(exist_ok=True)
 
-	result = f2c.fs2conf(dir_=empty_folder)
+    result = f2c.fs2conf(dir_=empty_folder)
 
-	assert result == {}
+    assert result == {}
