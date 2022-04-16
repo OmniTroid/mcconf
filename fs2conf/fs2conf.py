02a0d60720f97b890f643c9e15e7556fe89b76b3
David Skoland <davidskoland@gmail.com>
2022-04-16 12:23:24 +0200

    Reformat fs2conf.py

--- fs2conf/fs2conf.py
+++ fs2conf/fs2conf.py
@@ -5,76 +5,76 @@ from .dictcombiner import utils as dc
 
 
 # Combine all files in the given directory
-def combine_confs(conf_dir : Path) -> dict:
-	# This means the file has no config, which is perfectly valid
-	if not conf_dir.exists():
-		print('INFO: ' + str(conf_dir) + ' not found.')
-		return {}
+def combine_confs(conf_dir: Path) -> dict:
+    # This means the file has no config, which is perfectly valid
+    if not conf_dir.exists():
+        print('INFO: ' + str(conf_dir) + ' not found.')
+        return {}
 
-	# Determine how to parse this mess
-	if conf_dir.name.endswith('.json'):
-		read_func = fh.parse_json
-	elif conf_dir.name.endswith('.yml'):
-		read_func = fh.parse_yml
-	elif conf_dir.name.endswith('.properties'):
-		read_func = fh.parse_properties
-	else:
-		print('Error: unknown format for file: ' + conf_dir.name)
-		read_func = None
+    # Determine how to parse this mess
+    if conf_dir.name.endswith('.json'):
+        read_func = fh.parse_json
+    elif conf_dir.name.endswith('.yml'):
+        read_func = fh.parse_yml
+    elif conf_dir.name.endswith('.properties'):
+        read_func = fh.parse_properties
+    else:
+        print('Error: unknown format for file: ' + conf_dir.name)
+        read_func = None
 
-	subconf_files = sorted(
-		[Path(filename) for filename in conf_dir.iterdir()],
-		key=lambda f: f.name)
+    subconf_files = sorted(
+        [Path(filename) for filename in conf_dir.iterdir()],
+        key=lambda f: f.name)
 
-	subconfs = []
+    subconfs = []
 
-	for subconf_file in subconf_files:
-		subconfs.append(read_func(subconf_file))
+    for subconf_file in subconf_files:
+        subconfs.append(read_func(subconf_file))
 
-	result_conf = dc.combine_dicts(subconfs)
+    result_conf = dc.combine_dicts(subconfs)
 
-	return result_conf
+    return result_conf
 
 
 # Traverses the given dir and build a fully combined conf tree
 def fs2conf(dir_: Path) -> dict:
-	if not dir_.is_dir():
-		raise NotADirectoryError(dir_)
+    if not dir_.is_dir():
+        raise NotADirectoryError(dir_)
 
-	conf = {}
+    conf = {}
 
-	for child in dir_.iterdir():
-		subconf = {}
+    for child in dir_.iterdir():
+        subconf = {}
 
-		if child.is_file():
-			if child.name.endswith('.json'):
-				subconf = fh.parse_json(child)
+        if child.is_file():
+            if child.name.endswith('.json'):
+                subconf = fh.parse_json(child)
 
-		elif child.is_dir():
-			# This means the dir contains a set of files to be combined to one
-			if '.' in child.name:
-				subconf = combine_confs(child)
-			else:
-				subconf = fs2conf(child)
-		else:
-			print('Error: not a file or directory! ' + child.name)
+        elif child.is_dir():
+            # This means the dir contains a set of files to be combined to one
+            if '.' in child.name:
+                subconf = combine_confs(child)
+            else:
+                subconf = fs2conf(child)
+        else:
+            print('Error: not a file or directory! ' + child.name)
 
-		conf[child.name] = subconf
+        conf[child.name] = subconf
 
-	return conf
+    return conf
 
 
 # Combine the given list of paths to a dict
-def combine_dirs(dirs : [Path]) -> dict:
-	confs = []
+def combine_dirs(dirs: [Path]) -> dict:
+    confs = []
 
-	for dir_ in dirs:
-		if not dir_.is_dir():
-			print('Warning: ' + str(dir_) + ' is not a directory.')
-			continue
+    for dir_ in dirs:
+        if not dir_.is_dir():
+            print('Warning: ' + str(dir_) + ' is not a directory.')
+            continue
 
-		confs.append(fs2conf(dir_))
+        confs.append(fs2conf(dir_))
 
-	result_conf = dc.combine_dicts(confs)
+    result_conf = dc.combine_dicts(confs)
 
-	return result_conf
+    return result_conf
