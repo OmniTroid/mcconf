ad5193da98dfbf523dfef4a499ce31bc7a9f5fa8
David Skoland <davidskoland@gmail.com>
2023-09-30 12:19:02 +0200

    Add bungeecord plugin dir to template

--- coreconf_sample.py
+++ coreconf_sample.py
@@ -1,6 +1,7 @@
 coreconf = {
 	'java_dir': '/data/java',
 	'bukkit_plugin_dir': '/data/mc/plugins',
+	'bungeecord_plugin_dir': '/data/mc/plugins/bungeecord',
 	'launcher_dir': '/data/mc/launchers',
 	'roles_dir': '/data/mc/conf/roles',
 
